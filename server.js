1badb663ac3dbe7c07faa054070727af72cbc457
Dee Lynn <deannalguzman@gmail.com>
2019-10-31 13:19:12 -0700

    changed server.js routes

--- server.js
+++ server.js
@@ -11,25 +11,19 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static("app/public"));
 
 // Routes
 // =============================================================
-const routes =  require("./routes");
-app.use(routes);
+require("./app/routes/api-routes.js")(app);
 
-// Syncing our sequelize models and then starting our Express app
+// Starts the server to begin listening
 // =============================================================
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
