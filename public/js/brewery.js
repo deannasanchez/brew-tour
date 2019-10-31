6a7212199e02c3c390f5dd0a072cf688c84548ec
Dillon Couchois <dcouchois@gmail.com>
2019-10-31 13:51:07 -0700

    got map up

--- public/js/brewery.js
+++ public/js/brewery.js
@@ -16,8 +16,23 @@
 
 // Dillon's Code
 
+var latitude = 33.6461
+var longitute = -117.8425
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    var uluru = { lat: latitude, lng: longitute };
+    // The map, centered at Uluru
+    map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 10, center: uluru });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({ position: uluru, map: map });
+    // gmarkers.push(marker)
+}
 
 
+console.log(initMap);
 
 
 // 
