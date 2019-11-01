505b0819e7fe26f185da75df301261b709872ecd
Dillon Couchois <dcouchois@gmail.com>
2019-11-01 13:57:49 -0700

    installed nodemon and got static folder working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,23 +1,17 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
 
 // Dependencies
 // =============================================================
 var path = require("path");
 var router = require("express").Router();
-var fs = require("fs");
 
 // Each of the below routes just handles the HTML page that the user gets sent to.
-<<<<<<< HEAD
 // index route loads view.html
 router.get("/", function (req, res) {
-  res.render(path.join(__dirname, "../public/views/brewerySearch.html"));
-=======
-
-// index route loads view.html
-router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../views/breweySearch.html"));
->>>>>>> 20ed78ea3f0c0fa242642f38c90cf0c67c582f38
+  res.sendFile(path.join(__dirname, "../views/brewerySearch.html"));
 });
-
 
 // // cms route loads cms.html
 // router.get("/cms", function (req, res) {
