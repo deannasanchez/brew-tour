6e6a592181dead44efb06d9b7bb1989bfae464c9
Dillon Couchois <dcouchois@gmail.com>
2019-11-01 14:31:20 -0700

    DO NOT USE

--- public/js/axioscode.js
+++ public/js/axioscode.js
@@ -12,40 +12,40 @@ function runScript(e) {
 runScript();
 
 var search = $(".user-search")
-var queryURL = "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries?" + search;
+var queryURL = "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries?";
 
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
   console.log(response);
-  console.log(response.Runtime);
+  console.log(response.breweries.id);
 });
 
 
 
-// var unirest = require("unirest");
+var unirest = require("unirest");
 
-// var req = unirest("GET", "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries");
+var req = unirest("GET", "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries");
 
-// req.query({
-// 	"by_state": "NY",
-// 	"by_name": "cooper",
-// 	"by_tag": "patio",
-// 	"by_type": "micro"
-// });
+req.query({
+	"by_state": "NY",
+	"by_name": "cooper",
+	"by_tag": "patio",
+	"by_type": "micro"
+});
 
-// req.headers({
-// 	"x-rapidapi-host": "brianiswu-open-brewery-db-v1.p.rapidapi.com",
-// 	"x-rapidapi-key": "4c2c648dcfmsh26fbd934be4b150p1a0be1jsn56e7feae726d"
-// });
+req.headers({
+	"x-rapidapi-host": "brianiswu-open-brewery-db-v1.p.rapidapi.com",
+	"x-rapidapi-key": "4c2c648dcfmsh26fbd934be4b150p1a0be1jsn56e7feae726d"
+});
 
 
-// req.end(function (res) {
-// 	if (res.error) throw new Error(res.error);
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
 
-// 	console.log(res.body);
-// });
+	console.log(res.body);
+});
 
 
 
