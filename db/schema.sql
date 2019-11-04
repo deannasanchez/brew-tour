c6e1fdd126e55c357d51b44b3dc724a187a4b296
Roman <rom.zaikin@gmail.com>
2019-11-04 15:36:03 -0800

    Updates

--- db/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS brewery_db;
-
-CREATE DATABASE brewery_db;
--- routes/api/index.js
+++
@@ -1,6 +0,0 @@
-var router = require("express").Router();
-
-const brewinfoRoutes = require("./brewinfo-routes");
-router.use("/posts", brewinfoRoutes);
-
-module.exports = router;
