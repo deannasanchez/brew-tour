dd2fc0765e2cc81880f944b8f720f55c139e8825
Roman <rom.zaikin@gmail.com>
2019-11-04 16:44:13 -0800

    Final working

---
+++ controllers/brewinfo-controller.js
@@ -0,0 +1,90 @@
+// Roman's code
+
+
+
+
+// 
+
+
+// Dee's Code
+var db = require("../models");
+
+module.exports = brewController = {
+    read: function (req, res) {
+        var query = {};
+        if (req.query.brewery_id) {
+            query.BreweryId = req.query.brewery_id;
+        }
+        // Here we add an "include" property to our options in our findAll query
+        // We set the value to an array of the models we want to include in a left outer join
+        // In this case, just db.Author
+        db.breweryinfo.findAll({
+            where: query,
+            include: [db.Brewery]
+        }).then(function (dbbrweryinfo) {
+            res.json(dbbrweryinfo);
+        });
+    },
+    readById: function (req, res) {
+        // Here we add an "include" property to our options in our findOne query
+        // We set the value to an array of the models we want to include in a left outer join
+        // In this case, just db.Author
+        db.breweryinfo.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.breweries]
+        }).then(function (dbbreweryinfo) {
+            res.json(dbbreweryinfo);
+        });
+    },
+    create: function (req, res) {
+        db.breweryinfo.create(req.body).then(function (dbbreweryinfo) {
+            res.json(dbbreweryinfo);
+        });
+    },
+    update: function (req, res) {
+        db.breweryinfo.update(
+            req.body,
+            {
+                where: {
+                    id: req.body.id
+                }
+            }
+        ).then(function (dbbreweryinfo) {
+            res.json(dbbreweryinfo);
+        });
+    },
+    delete: function (req, res) {
+        db.breweryinfo.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbbreweryinfo) {
+            res.json(dbbreweryinfo);
+        });
+    }
+}
+
+
+
+
+// 
+
+// Dillon's Code
+
+
+
+
+
+// 
+
+//  Joseph's Code
+
+
+
+
+
+
+
+// 
--- public/js/axioscode.js
+++ public/js/axioscode.js
@@ -1,4 +1,3 @@
-
 var queryString = "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries?by_state=California"
 $.ajax({
   method: "GET",
@@ -28,8 +27,17 @@ function getBreweryData(input) {
     .then(function (response) {
       var sideBar = $("#search-results");
       sideBar.empty();
+      var obj = {
+        name: response[0].name,
+        addy: response[0].street,
+        city: response[0].city,
+        state: response[0].state,
+        web: response[0].website_url
+      }
+      console.log(obj)
+      $.post("/api/saved", obj)
       for (var i = 0; i < response.length; i++) {
-        var sideBarChild = $("<div id = 'sidebar-div'>" + (parseInt(i) + 1) + "." + " Name: " + response[i].name + "<br>" + "Address: " + response[i].street + "<br>" + response[i].city + "<br>" + response[i].state + "<br>" + "Website: " + "<a href=" + response[i].website_url + ">" + response[i].website_url + "</a>" + "<br>" + "<button id = 'save-button'> Save </button>" + "</div> <br>");
+        var sideBarChild = $("<div id = 'sidebar-div'>" + (parseInt(i) + 1) + "." + " Name: " + response[i].name + "<br>" + "Address: " + response[i].street + "<br>" + response[i].city + "<br>" + response[i].state + "<br>" + "Website: " + "<a href=" + response[i].website_url + ">" + response[i].website_url + "</a>" + "</div> <br>");
         sideBarChild.css('display', 'none');
         sideBar.append(sideBarChild);
         sideBarChild.show('slow');
@@ -63,30 +71,4 @@ function getBreweryData(input) {
       // .catch(function (error) {
       //   return error
       // });
-    }
-    )
-}
-
-
-
-  // axios.get('/', {
-  //   params: {
-  //     id: 299,
-  //     name: "Almanac Beer Company",
-  //     brewery_type: "micro",
-  //     street: "651B W Tower Ave",
-  //     city: "input",
-  //     state: "California",
-  //     postal_code: "94501-5047",
-  //     longitude: "-122.306283180899",
-  //     latitude: "37.7834497667258",
-  //     website_url: "http://almanacbeer.com",
-  //   }
-  // })
-  // .then(function (response) {
-  //   console.log(response);
-  // })
-  // .catch(function (error) {
-  //   console.log(error);
-  // });
-
+  // }
